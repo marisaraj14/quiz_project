0eb0024104a474c7e10d31fed2a373d1df25955b
joe <joe@onebillsoftware.com>
2021-10-25 23:47:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://i.ibb.co/CK33Rbm/image-1-removebg-preview.png" class="h-56 m-0  z-30 m-16" alt="Egg Fairy" />
+        <div class="border-dashed border-4 border-yellow-400 m-3 w-3/5 h-60">
+          <p class="font-TheGoodMonolith text-3xl">Um Hi, would you like me to quiz ya?</p>
+        </div>
       </header>
     </div>
   );
